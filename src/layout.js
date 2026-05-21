a25e08bb4fc4b11189a80b351c6968fa0b4dbf4b
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-05-21 12:12:43 +0600

    Fix MongoDB connection issue

--- src/layout.js
+++ src/layout.js
@@ -4,10 +4,6 @@ import NavBar from "@/components/sheared/NavBar";
 import Footer from "@/components/sheared/Footer";
 import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
