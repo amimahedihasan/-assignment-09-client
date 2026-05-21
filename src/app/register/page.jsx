839a1a7c462e603fd31729b66187b436f443be5b
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-05-21 12:06:21 +0600

    Create doctor cards UI

---
+++ src/app/register/page.jsx
@@ -0,0 +1,14 @@
+import RegisterForm from '@/components/RegisterForm';
+
+export const metadata = {
+    title: "DocAppoint || Register",
+};
+const page = () => {
+    return (
+        <div>
+            <RegisterForm />
+        </div>
+    );
+};
+
+export default page;
