1952e3fe15ebd21bbb784cda224134dbe634266d
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-05-21 12:07:21 +0600

    Add doctor details page

---
+++ src/log-in/page.jsx
@@ -0,0 +1,13 @@
+import LogInForm from '@/components/LogInForm';
+export const metadata = {
+    title: "DocAppoint || Log in",
+};
+const page = () => {
+    return (
+        <div>
+            <LogInForm/>
+        </div>
+    );
+};
+
+export default page;
